60a473960e04a02fb6ddba20c249d3fb1cd3d7f2
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-06-12 11:49:46 -0700

    Create n-curry.js

---
+++ n-curry.js
@@ -0,0 +1,7 @@
+// This function takes 1 argument n and returns a function that takes n function calls to fully unravel.
+// usage: 
+//   f(2)()
+//   > x=>y
+//   f(2)()()
+//   > x=>x
+f=(x,y=x=>x)=>x?f(x-1,x=>y):y
