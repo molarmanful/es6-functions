495c5dc04f8f8ffbcd43b1a533b9509d6c3dae62
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-06-13 16:16:10 -0700

    Update get-order.js

--- get-order.js
+++ get-order.js
@@ -1,3 +1,10 @@
 // This function gets the amount of times it takes to fully unravel a function.
+// Usage:
+// f(x=>x)
+// > 1
+// f(x=>x=>x)
+// > 2
+// f(x=>x=>x=>x)
+// > 3
 
 f=(x,y=0)=>x?f(x(),y+1):y
